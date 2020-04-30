ac9031d6f6cb6d1ca8fce459291a41dfeff348ad
Tutvydas Dulskis <tautvydas@dulskis.eu>
2020-04-30 23:24:48 +0300

    Changed File ending in entrypoint file

--- project/Docker/docker-php-entrypoint.sh
+++ project/Docker/docker-php-entrypoint.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-set -e
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-    set -- apache2-foreground "$@"
-fi
-
-exec "$@"
+#!/bin/shset -e# first arg is `-f` or `--some-option`if [ "${1#-}" != "$1" ]; then    set -- apache2-foreground "$@"fiexec "$@"
